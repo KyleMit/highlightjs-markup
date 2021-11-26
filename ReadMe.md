32f30ce9234d29b331d56106ac5ba5246eba49d1
Kyle <KyleMit@gmail.com>
2021-11-26 14:51:13 -0500

    update highlight version

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# HighlightJS Markup
+
+> Highlighting HighlightJS Syntax Highlighter
+
+Builds a `.html` file from the `index.js` file
+
+Using the [HighlightJS Node API](https://highlightjs.readthedocs.io/en/latest/api.html)
--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const hljs = require("highlight.js")
+import { promises as fs } from "fs";
+import hljs from "highlight.js";
+import Window from 'window';
+const { highlight, highlightElement } = hljs
 
 main()
 
 async function main() {
 
-    let lang = "html"
-    let htmlCode = `<a href="url"> Link </a>`
-    let htmlRaw = `&lt;a href="<mark>url</mark>"&gt; Link &lt;/a&gt;`
+    const lang = "html"
+    const htmlCode = `<a href="url"> Link </a>`
+    const htmlRaw = `&lt;a href="<mark>url</mark>"&gt; Link &lt;/a&gt;`
 
-    let reg = highlightCode(lang, htmlCode)
-    let raw = highlightRaw(lang, htmlRaw)
+    const reg = highlightCode(lang, htmlCode)
+    const raw = highlightRaw(lang, htmlRaw)
 
-    console.log(reg)
-    console.log(raw)
+    const html = `
+<html>
+<title>HighlightJS Demo</tile>
+<body>
 
-    let html = `
-    <html>
-    <body>
-    
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.9/styles/github.min.css" />
-    
-    <h3>Highlight Code</h3>
-    <pre><code class="language-${lang} hljs">${reg}</code></pre>
-    
-    <h3>Highlight Raw</h3>
-    <pre><code class="language-${lang} hljs">${raw}</code></pre>
-    
-    </body>
-    </html>
-    `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/github.min.css" />
 
-    const { promises: fs } = require("fs");
+<h3>Highlight Code</h3>
+<pre><code class="language-${lang} hljs">${reg}</code></pre>
+
+<h3>Highlight Raw</h3>
+<pre><code class="language-${lang} hljs">${raw}</code></pre>
+
+</body>
+</html>
+`
 
     await fs.writeFile("index.html", html)
 
@@ -38,24 +37,21 @@ async function main() {
 
 
 function highlightCode(lang, value) {
-    let codeSourceHighlight = hljs.highlight(lang, value).value
-    return codeSourceHighlight
+    const result = highlight(value, { language: lang})
+    return result.value
 }
 
 function highlightRaw(lang, value) {
     // get window methods
-    const Window = require('window');
-    const window = new Window();
-
-    // make document available globally
-    document = window.document
+    const { document } = new Window();
 
     // create text as node
-    let pre = window.document.createElement('pre');
+    const pre = document.createElement('pre');
     pre.innerHTML = `<code class="language-${lang}">${value}</code>`
-    let node = pre.childNodes[0]
+    const node = pre.childNodes[0]
 
-    hljs.highlightBlock(node)
+    // https://github.com/highlightjs/highlight.js/issues/2886
+    highlightElement(node)
 
     return node.innerHTML;
 }
